c2ed44e7835aac4e4cbd7a9c187f70bf8ad1a546
Zied Ben Bouallegue <77432122+zboualle@users.noreply.github.com>
2025-06-30 13:28:00 +0100

    set decode_timedelta=True

--- seeps4all/verification/utils/utils_data.py
+++ seeps4all/verification/utils/utils_data.py
@@ -22,7 +22,7 @@ def get_obs(path_data,obs_type,obs_origin="ecad",years="2022_2024",param="tp24")
             os.makedirs(path_data)
         print(f". fetching data from {url}")
         bucket = "ecmwf-rodeo-benchmark"
-        ds = xr.open_zarr(f'{url}/{bucket}/seeps4all/obs_{obs_type}_{param}_{years}_{obs_origin}.zarr')
+        ds = xr.open_zarr(f'{url}/{bucket}/seeps4all/obs_{obs_type}_{param}_{years}_{obs_origin}.zarr',decode_timedelta=True)
         ds.to_zarr(path_zarr)
     print(f"open: {path_zarr}")
     obs_seeps_data = xr.open_zarr(path_zarr,decode_timedelta=True)
@@ -41,7 +41,7 @@ def get_fct(path_data,name_forecasts):
         path_zarr=f"{path_data}/{name_forecasts[iex]}.zarr"
         if  os.path.isdir(path_zarr) == False:
             print(f". get data from {url}")
-            ds = xr.open_zarr(f'{url}/{bucket}/seeps4all/{name_forecasts[iex]}.zarr')
+            ds = xr.open_zarr(f'{url}/{bucket}/seeps4all/{name_forecasts[iex]}.zarr',decode_timedelta=True)
             ds.to_zarr(path_zarr)
         else:    
             print(f"open: {path_zarr}")
